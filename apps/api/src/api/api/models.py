ab22ffb28256d708e1b1b50b384790bcc302b132
Tzvetlin Velev <tvelev@ameripharma.us>
2026-07-04 14:55:16 -0700

    week-2 completed

--- apps/api/src/api/api/models.py
+++ apps/api/src/api/api/models.py
@@ -1,7 +1,16 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class RagRequest(BaseModel):
     query: str
 
+
+class RAGUsedContext(BaseModel):
+    image_url: str
+    price: Optional[float] = None
+    description: str
+
 class RagResponse(BaseModel):
     answer: str
+    used_context: list[RAGUsedContext]
+     
